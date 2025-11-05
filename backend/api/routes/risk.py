33b8a111a4f085ff41f8c0f2b4024387718ed006
freemandaily <onahchinemelem@gmail.com>
2025-11-06 00:55:29 +0100

    Added celery Framwork

--- backend/api/routes/risk.py
+++ backend/api/routes/risk.py
@@ -1,10 +1,37 @@
 
+# from fastapi import APIRouter
+
+# router = APIRouter()
+
+# @router.get('/risk')
+# async def risk():
+
+#     # logic
+#     return {'agent':'risk','status':'ok'}
+
+
+
 from fastapi import APIRouter
+from tasks import risk_task
+
+from celery.result import AsyncResult
+from core import celery_app
 
 router = APIRouter()
 
 @router.get('/risk')
 async def risk():
-
+    task = risk_task.delay() # adding the background worker
     # logic
-    return {'agent':'risk','status':'ok'}
+    return {'agent':'risk ','task_id': task.id}
+
+# fetching the results
+@router.get('/risk/status/{task_id}')
+async def get_risk_result(task_id:str):
+    task_result = AsyncResult(task_id, app=celery_app)
+
+    return {
+        "task_id": task_id,
+        "status": task_result.status,
+        "result": task_result.result,
+    }
