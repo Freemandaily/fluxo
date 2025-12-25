62d99c2c9460d447b9cb1d17ce150c9e32517b7d
Freeman <107298762+Freemandaily@users.noreply.github.com>
2025-12-25 21:42:03 +0100

    Update main.py

--- main.py
+++ main.py
@@ -52,42 +52,42 @@ app = FastAPI(
 
 
 
-# # Attach Al Agent Routes
-# app.include_router(automation_router,prefix='/agent')
-# app.include_router(execution_router,prefix='/agent')
-# app.include_router(governance_router,prefix='/agent')
-# app.include_router(macro_router,prefix='/agent')
-# app.include_router(market_data_router,prefix='/agent')
-# #app.include_router(portfolio_router,prefix='/agent')
-# app.include_router(research_router,prefix='/agent')
-# app.include_router(risk_router,prefix='/api/agent/risk',tags=['Risk'])
-# app.include_router(x402_router,prefix='/agent',tags=['x402'])
-# app.include_router(onchain_router,prefix='/api/agent/onchain',tags=['Onchain'])
-# app.include_router(yield_router,prefix='/agent')
-# app.include_router(social_router,prefix='/agent')
-# app.include_router(alerts_router, prefix="/api/alerts", tags=["Alerts"])
-# app.include_router(portfolio_router, prefix="/api/v1/agent", tags=["portfolio"])
-# app.include_router(system_router, prefix="/api/v1/system", tags=["system"])
-# app.include_router(digest_router, prefix="/api/v1/daily", tags=["market-update"])
-# app.include_router(whale_router, prefix="/agent/whale", tags=["Whale"])
+# Attach Al Agent Routes
+app.include_router(automation_router,prefix='/agent')
+app.include_router(execution_router,prefix='/agent')
+app.include_router(governance_router,prefix='/agent')
+app.include_router(macro_router,prefix='/agent')
+app.include_router(market_data_router,prefix='/agent')
+#app.include_router(portfolio_router,prefix='/agent')
+app.include_router(research_router,prefix='/agent')
+app.include_router(risk_router,prefix='/api/agent/risk',tags=['Risk'])
+app.include_router(x402_router,prefix='/agent',tags=['x402'])
+app.include_router(onchain_router,prefix='/api/agent/onchain',tags=['Onchain'])
+app.include_router(yield_router,prefix='/agent')
+app.include_router(social_router,prefix='/agent')
+app.include_router(alerts_router, prefix="/api/alerts", tags=["Alerts"])
+app.include_router(portfolio_router, prefix="/api/v1/agent", tags=["portfolio"])
+app.include_router(system_router, prefix="/api/v1/system", tags=["system"])
+app.include_router(digest_router, prefix="/api/v1/daily", tags=["market-update"])
+app.include_router(whale_router, prefix="/agent/whale", tags=["Whale"])
 
 
-# # NOTE: Payment middleware commented out for local development
-# app.middleware("http")(
-#     require_payment(
-#         price="$0.01",
-#         pay_to_address='0xEd04925173FAD6A8e8939338ccF23244cae1fF12',
-#         path='/api/v1/daily/digest',
-#         network='base-sepolia',
-#         facilitator_config=FacilitatorConfig(
-#             url= "https://x402.treasure.lol/facilitator"
-#         ),
-#         # paywall_config=PaywallConfig(
-#         #     cdp_client_key='==',
-#         #     app_name='fluxo',
-#         # )
-#     )
-# )
+# NOTE: Payment middleware commented out for local development
+app.middleware("http")(
+    require_payment(
+        price="$0.01",
+        pay_to_address='0xEd04925173FAD6A8e8939338ccF23244cae1fF12',
+        path='/api/v1/daily/digest',
+        network='base-sepolia',
+        facilitator_config=FacilitatorConfig(
+            url= "https://x402.treasure.lol/facilitator"
+        ),
+        # paywall_config=PaywallConfig(
+        #     cdp_client_key='==',
+        #     app_name='fluxo',
+        # )
+    )
+)
 
 
 @app.get("/")
